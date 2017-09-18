b0eafd391f4702c039ff18f6914c2ed22d12cddb
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-09-18 10:26:42 -0300

    Created location model

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lon = models.DecimalField(max_digits=9, decimal_places=6)
+    address = models.CharField(max_length=120)
