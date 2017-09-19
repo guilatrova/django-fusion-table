2063c97e887ac2f6f7bdf5928f2106230b84a250
Guilherme Latrova <guilhermelatrova@hotmail.com>
2017-09-19 13:50:32 -0300

    Migration: address is unique

--- locations/models.py
+++ locations/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Location(models.Model):
     lat = models.DecimalField(max_digits=50, decimal_places=20)
     lon = models.DecimalField(max_digits=50, decimal_places=20)
-    address = models.CharField(max_length=120)
+    address = models.CharField(max_length=120, unique=True)
 
     class Meta:
         unique_together = ('lat', 'lon',)
